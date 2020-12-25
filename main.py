4fa610c258abfdde81487e3049f59c6c35bf05f6
surrealsymmetry <surrealsymmetry@gmail.com>
2020-12-25 20:09:34 -0330

    functional, no ID

--- main.py
+++ main.py
@@ -1,59 +1,65 @@
+import random
+
+
 class Entity:
-    def __init__(self, id):
-        print("\tE\tinstance initiated with id",id)
-        assert type(id) is int
-        self.id = id
+    def __init__(self):
+        # print("\tE\tinstance initiated")                           #cc
         self.components = {}
 
     def grant(self, c):
         self.components[c.key] = c
-        print("\tE\t%s\tgranted C\t'%s'\t%s" % (self.id, c.key, c.id))
+        c.entity = self
+        # print("\tE\tgranted C\t'%s'" % (c.key))                    #cc
         c.entity = self
         return c
 
 
 class Component:
-    def __init__(self, key, id):
-        print("\tC\tinstance initiated with id %s and key '%s'" % (id, key))
+    def __init__(self, key):
+        # print("\tC\tinstance initiated w/ key '%s'" % (key))       #cc
         assert type(key) is str
-        assert type(id) is int
         self.key = key
-        self.id = id
 
 
 class Rack:
 
     def __init__(self):
-        print("\tR\tinstance initiated")
-        self.entities = {}
+        # print("\tR\tinstance initiated")                           #cc
+        self.entities = []
         self.components = {}
-        self.e_id = 0
-        self.c_id = 0
 
-    def __repr__(self):
-        element_totals = "\tE:\t"+str(self.e_id)+"\n\tC:\t"+str(self.c_id)
-        return element_totals
+    def __repr__(self):  # overrides the output of print(rack_object)
+        component_counter = 0
+        key_list = ""
+        tally_dict = {}
+        for key in self.components:
+            component_counter += len(self.components[key])
+            if key not in tally_dict.keys():
+                tally_dict[key] = 0
+            tally_dict[key] += len(self.components[key])
+            key_list = key_list + ("\n\t\t\t\t" + str(tally_dict[key]) + "  '" + str(key)+"'")
+        msg = "\tE:\t" + str(len(self.entities)) + "\n\tC:\t" + str(len(self.components)) + " keys\t" + str(
+            component_counter) + " components" + key_list
+        return msg
 
     def request_entity(self):
-        e = Entity(self.e_id)
-        self.e_id += 1
-        self.entities.update({self.e_id: e})
+        e = Entity()
+        self.entities.append(e)
         return e
 
     def request_component(self, key):
-        c = Component(key, self.c_id)
-        self.c_id += 1
-        if c.key in self.components.keys() :
-            self.components[c.key] = c
-            print("\tC:\t",c.key,"racked")
+        c = Component(key)
+        if c.key in self.components.keys():
+            self.components[c.key].append(c)  # appending to the table that is the value of the key
+            # print("\tC:\t",c.key,"racked")             #cc
         else:
-            self.components.update({c.key:[c]})
-            print("\tC:\track entry '%s' added" % (c.key))
+            self.components.update({c.key: [c]})
+            print("\track entry '%s' added" % (c.key))
         return c
 
 
 print("Creating instance of class Rack")
 my_rack = Rack()
 my_entity = my_rack.request_entity()
-my_entity.grant(my_rack.request_component("dummy"))
+
 print(my_rack)
